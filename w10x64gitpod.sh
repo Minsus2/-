d22deec2ce32adcf92703cae5cc63ad1e35b9ab0
ankdc <130301108+ankdc@users.noreply.github.com>
2023-04-10 17:35:27 +0700

    Update w10x64gitpod.sh

--- w10x64gitpod.sh
+++ w10x64gitpod.sh
@@ -1,27 +1,13 @@
 apt-get update
-echo "Download windows files"
 wget -O w10x64.img https://bit.ly/akuhnetW10x64
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://raw.githubusercontent.com/ankdc/-/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
 ./ngrok tcp --region ap 3388 &>/dev/null &
 clear
-echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
-echo "Wait"
-echo "Starting Windows"
 qemu-system-x86_64 -hda w10x64.img -m 25G -smp cores=2 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
@@ -29,12 +15,5 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "===================================="
 echo "Username: akuh"
 echo "Password: Akuh.Net"
-echo "===================================="
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free RDP now"
-echo "Wait 2 minute to finish bot"
-echo "You can close this tab"
-echo "RDP runs for 50 hours"
 echo "===================================="
 sleep 432000
